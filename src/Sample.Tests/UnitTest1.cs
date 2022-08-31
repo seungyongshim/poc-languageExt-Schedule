40b3b416dbbf83e983d6cf6498be1335057aee72
심승용 <seungyongshim@nexon.co.kr>
2022-08-31 19:41:33 +0900

    Schedule과 ScheduleTransformer는 완전 다른것

--- src/Sample.Tests/UnitTest1.cs
+++ src/Sample.Tests/UnitTest1.cs
@@ -1,14 +1,45 @@
 using System;
+using FluentAssertions;
+using LanguageExt;
 using Xunit;
-using static Sample.Prelude;
+using static LanguageExt.Prelude;
 
 namespace Sample.Tests;
 
 public class PreludeSpec
 {
     [Fact]
-    public void AddSuccess()
+    public void Exponential1()
     {
-        Assert.Equal(3, add(1)(2));
+        var t = Schedule.exponential(1 * sec) | Schedule.recurs(5);
+
+        var r = t.Run();
+
+        _ = r.Should().BeEquivalentTo(new Duration[]
+        {
+            1 * sec,
+            2 * sec,
+            4 * sec,
+            8 * sec,
+            16 * sec,
+        });
+    }
+
+    [Fact]
+    public void Fibonacci1()
+    {
+        var t = Schedule.fibonacci(1 * sec) | Schedule.recurs(6);
+
+        var r = t.Run();
+
+        _ = r.Should().BeEquivalentTo(new Duration[]
+        {
+            1 * sec,
+            1 * sec,
+            2 * sec,
+            3 * sec,
+            5 * sec,
+            8 * sec
+        });
     }
 }
