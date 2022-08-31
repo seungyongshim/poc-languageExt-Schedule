5865388af3137242ae397c6b5c912edaa73c227d
심승용 <seungyongshim@nexon.co.kr>
2022-08-31 20:00:38 +0900

    1

--- src/Sample.Tests/UnitTest1.cs
+++ src/Sample.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using LanguageExt;
 using Xunit;
@@ -40,6 +41,46 @@ public class PreludeSpec
             3 * sec,
             5 * sec,
             8 * sec
+        });
+    }
+
+    [Fact]
+    public void Fibonacci2()
+    {
+        var t = Schedule.fibonacci(1 * sec) | Schedule.maxDelay(5 * sec)| Schedule.recurs(8);
+
+        var r = t.Run();
+
+        _ = r.Should().BeEquivalentTo(new Duration[]
+        {
+            1 * sec,
+            1 * sec,
+            2 * sec,
+            3 * sec,
+            5 * sec,
+            5 * sec,
+            5 * sec,
+            5 * sec,
+        });
+    }
+
+    [Fact]
+    public async Task FixedInterval()
+    {
+        var t = Schedule.fixedInterval(3 * sec) | Schedule.recurs(8);
+
+        var r = t.Run();
+
+        _ = r.Should().BeEquivalentTo(new Duration[]
+        {
+            3 * sec,
+            3 * sec,
+            3 * sec,
+            3 * sec,
+            3 * sec,
+            3 * sec,
+            3 * sec,
+            3 * sec,
         });
     }
 }
